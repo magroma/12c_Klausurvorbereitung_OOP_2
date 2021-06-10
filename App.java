2db6836188d3d38971994d522685fdabaa24c616
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-06-10 10:28:50 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	Test test = new Test("Grizabella", 29);
+	
+	System.out.println(test.getAge());
+	System.out.println(test.getName());
+		
+	}
+
+}
